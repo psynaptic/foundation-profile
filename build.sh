516e83887fb7928b02b804f2afd9d045eec0c423
Balazs Dianiska (snufkin) <balazs@longlake.co.uk>
2010-03-30 23:12:11 +0100

    New build architecture for custom projects

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Build script that edits the skeleton profile file and generates 
+# a new one with project relevant details
+# Example: 
+# FRIENDLY_NAME="AP Installer"
+# NAME will be a machine-friendly name generated (so it can be used in function names).
+
+# Configuration
+FRIENDLY_NAME="AP Installer"
+DESCRIPTION="Install the AP website"
+
+# Do not change these values.
+
+machine_name() {
+  # Lowercasing all and white space fix.
+  echo "$1" | tr "[:upper:]" "[:lower:]" | tr "[:blank:]" "_"
+}
+
+NAME=`machine_name "$FRIENDLY_NAME"`
+INPUT='essential.profile'
+OUTPUT="$NAME.profile"
+
+# Creating a backup
+cp $INPUT $INPUT.old
+sed -i -r s/NAME/$NAME/ $INPUT
+
+mv $INPUT $OUTPUT
+
+
